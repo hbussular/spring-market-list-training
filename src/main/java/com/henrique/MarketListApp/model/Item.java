37f41b666e1d0b95a66f3a984f7329128cb66acc
Henrique Bussolari <hbussular@hotmail.com>
2024-02-22 10:22:47 -0300

    Creates Item Entity

---
+++ src/main/java/com/henrique/MarketListApp/model/Item.java
@@ -0,0 +1,16 @@
+package com.henrique.MarketListApp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity // diz que isso será uma entidade no banco
+@Data // Cria todos os getters e setters
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // diz que eh chave primaria e não tem ninguém igual a ela
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private Double price;
+}
