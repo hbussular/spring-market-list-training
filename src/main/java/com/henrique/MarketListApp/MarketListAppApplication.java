6712019be62eb56f6056695a18b718fd577a1657
Henrique Bussolari <hbussular@hotmail.com>
2024-02-22 10:23:34 -0300

    Main

---
+++ src/main/java/com/henrique/MarketListApp/MarketListAppApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.MarketListApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketListAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketListAppApplication.class, args);
+	}
+
+}
