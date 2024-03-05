81368dd6611364f27cd281cb758ebdb88e68c242
Henrique Bussolari <hbussular@hotmail.com>
2024-03-05 09:51:14 -0300

    Inserts validation annotations to the student class

--- src/main/java/com/henrique/SchoolApp/model/Student.java
+++ src/main/java/com/henrique/SchoolApp/model/Student.java
@@ -1,7 +1,12 @@
 package com.henrique.SchoolApp.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.br.CPF;
 
 @Entity // diz que isso será uma entidade no banco
 @Data // Cria todos os getters e setters
@@ -9,16 +14,25 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // diz que eh chave primaria e não tem ninguém igual a ela
     private Long id;
-    @Column
+
+    @Column @NotNull @NotEmpty @Length(max = 40)
     private String username;
+
     @Column
     private String password;
-    @Column
+
+    @Column @NotNull @NotEmpty @Email
     private String email;
+
     @Column
     private String name;
+
     @Column
     private String surname;
+
     @Column
     private Integer age;
+
+    @Column @NotNull @NotEmpty @CPF
+    private String cpf;
 }
