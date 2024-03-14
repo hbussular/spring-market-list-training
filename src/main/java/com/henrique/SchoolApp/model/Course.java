231da74920b596d2a7cb75f84e6ea9e7559c77f3
Henrique Bussolari <hbussular@hotmail.com>
2024-03-14 15:34:27 -0300

    Revert "Creates course entity and DTO"
    This reverts commit 7529b4eb897c1068a7fd2c402936b535eb982fa1.

--- src/main/java/com/henrique/SchoolApp/model/Course.java
+++ src/main/java/com/henrique/SchoolApp/model/Course.java
@@ -1,33 +1,5 @@
 package com.henrique.SchoolApp.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Course {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column @NotEmpty @NotNull
-    private String CourseName;
-
-    @Column @NotEmpty @NotNull
-    private LocalDate startDate;
-
-    @Column
-    private LocalDate endDate;
-
-    @Column
-    private Long grade;
-
+    private String name;
 }
