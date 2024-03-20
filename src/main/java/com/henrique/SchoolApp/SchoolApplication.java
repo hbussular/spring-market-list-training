3d8ffe2ce21904075fc40d11fca8984913a5dde1
Henrique Bussolari <hbussular@hotmail.com>
2024-03-20 14:31:32 -0300

    adds @EnableSpringDataWebSupport annotation to the main method in order to allow the usage of pageable and sort on the web controllers

--- src/main/java/com/henrique/SchoolApp/SchoolApplication.java
+++ src/main/java/com/henrique/SchoolApp/SchoolApplication.java
@@ -2,8 +2,10 @@ package com.henrique.SchoolApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class SchoolApplication {
 
 	public static void main(String[] args) {
