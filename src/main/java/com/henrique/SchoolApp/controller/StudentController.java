6fe48b60f28b7e1f346a096dfba411e423a01329
Henrique Bussolari <hbussular@hotmail.com>
2024-03-20 14:32:23 -0300

    Adds pageable and sort logic to the get all students method

--- src/main/java/com/henrique/SchoolApp/controller/StudentController.java
+++ src/main/java/com/henrique/SchoolApp/controller/StudentController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,10 +24,7 @@ public class StudentController {
     @Autowired
     private StudentRepository studentRepository;
     @GetMapping
-    public Page<StudentDto> getStudents(@RequestParam(required = false) String studentId,
-                                        @RequestParam int page,
-                                        @RequestParam int quantity) {
-        Pageable pagination = PageRequest.of(page, quantity);
+    public Page<StudentDto> getStudents(@RequestParam(required = false) String studentId, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pagination ) {
         Page<Student> students = studentRepository.findAll(pagination);
         return StudentDto.convert(students);
     }
