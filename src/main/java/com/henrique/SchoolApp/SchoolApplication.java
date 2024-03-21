54f52c6aa2abc7d56f9702d42fc538a26f4d68d4
Henrique Bussolari <hbussular@hotmail.com>
2024-03-21 10:51:12 -0300

    Enables caching on the main application

--- src/main/java/com/henrique/SchoolApp/SchoolApplication.java
+++ src/main/java/com/henrique/SchoolApp/SchoolApplication.java
@@ -2,10 +2,12 @@ package com.henrique.SchoolApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 public class SchoolApplication {
 
 	public static void main(String[] args) {
